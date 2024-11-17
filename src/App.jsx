dcf081d8d1dde2040fc7e1b4ed576abbdaff384a
poseitutu10 <phinehasoseitutu1@gmail.com>
2024-11-17 13:05:14 -0800

    header-fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Properties from "./pages/Properties";
+import Categories from "./pages/Categories";
+import Blogs from "./pages/Blogs";
+import Services from "./pages/Services";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/properties",
+    element: <Properties />,
+  },
+  {
+    path: "/categories",
+    element: <Categories />,
+  },
+  {
+    path: "/blogs",
+    element: <Blogs />,
+  },
+  {
+    path: "/services",
+    element: <Services />,
   },
 ]);
 export default function App() {
---
+++ src/pages/Properties.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MainHeader from "../components/MainHeader";
+
+const Properties = () => {
+  return (
+    <div>
+      <MainHeader />
+    </div>
+  );
+};
+
+export default Properties;
