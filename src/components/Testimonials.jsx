b2ec110d3bf1a3ca31646f7e8e4e3bad6e2a25ca
ebene4321@gmail.com <EnamAbra>
2024-11-22 11:29:10 +0000

    updates

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -34,27 +34,6 @@ const Testimonial = () => {
                 always remember this positive experience.
               </p>
             </div>
-            <div className="slide swiper-slide">
-              <img
-                src="/Servicesimages/darlenereview.jpg"
-                width={300}
-                height={300}
-              />
-              <div className="test-box">
-                <h4>Our Dream Home Became a Reality!</h4>
-                <p>
-                  The team at Mapel helped us find a home that we had only ever
-                  imagined. We were very particular about the kind of property
-                  we wanted and had high expectations, but Mapel delivered
-                  beyond what we thought was possible. Their commitment to
-                  finding the right home for us was evident in every
-                  interaction. Even after we moved in, they stayed in touch to
-                  ensure everything was perfect. We’re incredibly grateful for
-                  their efforts and will always remember this positive
-                  experience.
-                </p>
-              </div>
-            </div>
           </div>
         </div>
       </div>
